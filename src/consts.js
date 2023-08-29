647eed37cbb59e459ff8a3cca7d7c1e594ba2f58
Yan Carlos Morera <yanmorera1@gmail.com>
2023-08-28 22:16:03 -0500

    add: consts

---
+++ src/consts.js
@@ -0,0 +1,6 @@
+export const BASE_URL = 'https://localhost:7221/api'
+export const FORM_STATES = {
+    SUCCESS: Symbol('Success'),
+    FAILED: Symbol('Failed'),
+    INITIAL: Symbol('Initial'),
+}
